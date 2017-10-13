b2e34fdcebd9dd9167ccf8507c93619983555cc7
lihaoa <lihaoa123>
2017-10-13 22:32:29 +0800

    调用豆瓣数据渲染

--- js/app.js
+++ js/app.js
@@ -8,16 +8,59 @@ angular.module('app', ['ui.router'])
         controller: 'in_theatersCtrl'
       })
     $urlRouterProvider.otherwise('/in_theaters');
+    //配置angular白名单
+    /*$sceDelegateProvider.resourceUrlWhitelist([
+     // Allow same origin resource loads.
+     'self',
+     'https://'
+     ]);*/
   }])
+  .service('myService', function () {
 
-  .controller('in_theatersCtrl', ['$scope', '$http', function ($scope, $http) {
-    $http({
-      url: './js/in_theaters.json',
-      method: 'get'
-    }).then(function (res) {
-      console.log(res.data);
-      $scope.result = res.data;
+    this.myJsonp = function (url, data, callback) {
+
+      var fnName = 'myJsonp_' + Math.random().toString().replace('.', '');
+
+      window[fnName] = callback;
+
+      var querystring = '';
+
+      for (var attr in data) {
+
+        querystring += attr + '=' + data[attr] + '&';
+
+      }
+
+      var script = document.createElement('script');
+
+      script.src = url + '?' + querystring + 'callback=' + fnName;
+
+      script.onload = function () {
+
+        document.body.removeChild(script);
+
+      }
+
+      document.body.appendChild(script);
+
+    }
+
+  })
+  .controller('in_theatersCtrl', ['$scope', 'myService', function ($scope, myService) {
+    //$http({
+    //  url: './js/in_theaters.json',
+    //  method: 'get'
+    //}).then(function (res) {
+    //  console.log(res.data);
+    //  $scope.result = res.data;
+    //})
+    myService.myJsonp('https://api.douban.com/v2/movie/in_theaters', {}, function (res) {
+      // console.log(res)
+      $scope.result = res;
+      //由于是原生jsonp方法
+      $scope.$apply();
     })
+
   }])
   .controller('appCtrl', ['$scope', function ($scope) {
 
