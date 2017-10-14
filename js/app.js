6900e2607127ddf150c198ca23184b5910ef4c3a
lihaoa <lihaoa123>
2017-10-14 10:16:28 +0800

    修复bug完成正在热播与即将上映

--- js/app.js
+++ js/app.js
@@ -2,10 +2,22 @@ angular.module('app', ['ui.router'])
   .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state({
-        url: '/in_theaters',
+        url: '/in_theaters/:page',
         templateUrl: './view/in_theaters/in_theaters.html',
         name: 'in_theaters',
         controller: 'in_theatersCtrl'
+      })
+      .state({
+        url: '/search/:page/:searchInfo',
+        templateUrl: './view/search/search.html',
+        name: 'search',
+        controller:'searchCtrl'
+      })
+      .state({
+        url: '/comming_soon/:page',
+        templateUrl: './view/comming_soon/comming_soon.html',
+        name: 'comming_soon',
+        controller: 'comming_soonCtrl'
       })
     $urlRouterProvider.otherwise('/in_theaters');
     //配置angular白名单
@@ -46,7 +58,7 @@ angular.module('app', ['ui.router'])
     }
 
   })
-  .controller('in_theatersCtrl', ['$scope', 'myService', function ($scope, myService) {
+  .controller('in_theatersCtrl', ['$scope', 'myService', '$stateParams', '$location', function ($scope, myService, $stateParams, $location) {
     //$http({
     //  url: './js/in_theaters.json',
     //  method: 'get'
@@ -54,15 +66,137 @@ angular.module('app', ['ui.router'])
     //  console.log(res.data);
     //  $scope.result = res.data;
     //})
-    myService.myJsonp('https://api.douban.com/v2/movie/in_theaters', {}, function (res) {
-      // console.log(res)
+    //
+    //start	起始元素
+    //count	返回结果的数量
+    var page = $stateParams['page'];
+    var count = 9;
+    var start = (page - 1) * count;
+    var totalPage = 0;
+    myService.myJsonp('https://api.douban.com/v2/movie/in_theaters', {
+      count:count,
+      start:start
+    }, function (res) {
       $scope.result = res;
+      totalPage = Math.ceil(res.total/count);
       //由于是原生jsonp方法
       $scope.$apply();
     })
+    $scope.changePage = function(type){
+
+      if(type == 'next'){
+
+        // 下一页
+        page++;
+
+        if(page > totalPage){
+
+          page = totalPage
+
+        }
+
+      }else if(type == 'prev'){
+
+        // 上一页
+        page--;
+
+        if(page < 1){
+          page = 1;
+        }
+
+      }
+
+      // 跳转页面
+      $location.path('/in_theaters/'+page);
+
+    }
 
   }])
-  .controller('appCtrl', ['$scope', function ($scope) {
+
+  .controller('comming_soonCtrl', ['$scope', 'myService', '$stateParams', '$location', function ($scope, myService, $stateParams, $location) {
+    var page = $stateParams['page'];
+    var count = 9;
+    var start = (page - 1) * count;
+    var totalPage = 0;
+    myService.myJsonp('https://api.douban.com/v2/movie/coming_soon', {
+      count:count,
+      start:start
+    }, function (res) {
+      $scope.result = res;
+      totalPage = Math.ceil(res.total/count);
+      //由于是原生jsonp方法
+      $scope.$apply();
+    })
+    $scope.changePage = function(type){
+
+      if(type == 'next'){
+
+        // 下一页
+        page++;
+
+        if(page > totalPage){
+
+          page = totalPage
+
+        }
+
+      }else if(type == 'prev'){
+
+        // 上一页
+        page--;
+
+        if(page < 1){
+          page = 1;
+        }
+
+      }
+
+      // 跳转页面
+      $location.path('/comming_soon/'+page);
+
+    }
 
   }])
 
+  .controller('searchCtrl', ['$scope','myService', '$stateParams', '$location',function ($scope, myService, $stateParams,$location) {
+    var searchInfo = $stateParams.searchInfo;
+    var page = $stateParams['page'];
+    var count = 9;
+    var start = (page - 1) * count;
+    var totalPage = 0;
+    myService.myJsonp('https://api.douban.com/v2/movie/search', {
+      q:searchInfo,
+      count:count,
+      start:start
+    }, function (res) {
+      console.log(res)
+      $scope.result = res;
+      totalPage = Math.ceil(res.total/count);
+      //由于是原生jsonp方法
+      $scope.$apply();
+    })
+    $scope.changePage = function(type){
+      if(type == 'next'){
+        // 下一页
+        page++;
+        if(page > totalPage){
+          page = totalPage
+        }
+      }else if(type == 'prev'){
+        // 上一页
+        page--;
+        if(page < 1){
+          page = 1;
+        }
+
+      }
+      // 跳转页面
+      $location.path('/search/'+page+'/searchInfo');
+    }
+  }])
+  .controller('navCtrl',['$scope','$location',function ($scope,$location) {
+    $scope.search=function () {
+      $location.path('/search/1/'+$scope.searchInfo);
+      $scope.searchInfo = "";
+    }
+  }])
